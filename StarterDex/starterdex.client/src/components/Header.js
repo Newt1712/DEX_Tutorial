f1890d5aaed129753a234c755ccc34905c49e5f2
AnthonyKhieu <newt.the.creator@gmail.com>
2024-02-20 09:58:30 +0700

    init project

---
+++ StarterDex/starterdex.client/src/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Logo from '../moralis-logo.svg'
+import Eth from '../eth.svg'
+import {Link} from 'react-router-dom'
+function Header() {
+  return (
+    <header>
+      <div className='leftH'>
+        <img src={Logo} alt='Logo' className='logo' />
+        <Link to="/" className='link'>
+          <div className='headerItem'> Swap </div>
+        </Link>
+        <Link to="/tokens" className='link'>
+          <div className='headerItem'> Tokens </div>
+        </Link>
+      </div>
+      <div className='rightH'>      
+        <div className='headerItem'> 
+          <img src={Eth} alt='Eth' className='eth' />
+          Ethereum
+        </div>
+        <div className='connectButton' onClick> 
+          Connect
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
