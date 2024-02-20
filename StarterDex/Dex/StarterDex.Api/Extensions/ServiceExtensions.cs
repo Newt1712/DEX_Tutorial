bcaa1e07ad587a6e0318701b35ba7c337808bb34
AnthonyKhieu <newt.the.creator@gmail.com>
2024-02-20 09:24:26 +0700

    init project
    init project
    
    update project
    
    add folder and projects
    
    update code
    
    update project structure

---
+++ StarterDex/Dex/StarterDex.Api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using StarterDex.Domain.Settings;
+
+namespace StarterDex.Server.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var identitySettingsSection = configuration.GetSection("Moralis");
+            services.Configure<MoralisSettings>(identitySettingsSection);
+
+        }
+    }
+}
---
+++ StarterDex/Dex/StarterDex.Domain/Entities/TokenNativePrice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarterDex.Domain.Entities
+{
+    public class TokenNativePrice
+    {
+        public string Value { get; set; }
+        public int Decimals { get; set; }
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public string Address { get; set; }
+    }
+}
